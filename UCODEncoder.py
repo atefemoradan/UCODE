e2665c5f993c01a34131474c59a1deccbfccaa3f
au685399 <atefemoradan@gmail.com>
2022-11-06 17:24:07 +0100

    Encoder Cleanup

--- UCODEncoder.py
+++ UCODEncoder.py
@@ -8,15 +8,11 @@ class GCN(nn.Module):
         self.fc_2 = nn.Linear(hidden_size, out_ft, bias=False)
 
         self.batch = nn.BatchNorm1d(num_features=nb_nodes)
-        #self.batch_2 = nn.BatchNorm1d(num_features=nb_nodes)
 
 
         self.act_1=nn.SiLU()
         self.act_2=nn.RReLU()
-        #self.logsoftmax = nn.LogSoftmax(dim=2)
 
-        """self.bias = nn.Parameter(torch.FloatTensor(out_ft))
-        self.bias.data.fill_(0.0)"""
 
         for m in self.modules():
             self.weights_init(m)
@@ -36,14 +32,6 @@ class GCN(nn.Module):
         h = self.fc_2(h)
         h = torch.bmm(adj, h)
         h= self.batch(h)
-        #h = torch.abs(h)
         h=self.act_2(h)
-        # This works well but is not what we say in the theory section
 
-        #output = self.logsoftmax(h)
-
-        # This is what we say in the theory section but does not give as good results
-        # norm = torch.sum(h, dim=-1, keepdim=True)
-        # norm[norm <= 0.0001] = 1
-        # output = h / norm
         return h
